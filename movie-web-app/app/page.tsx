b77e26afa64a927eea122f9acc2c416c5b75f5dd
Khatanbaatar A. <25lp5598@nest.edu.mn>
2025-09-11 11:08:12 +0800

    dfghjk

--- movie-web-app/app/page.tsx
+++ movie-web-app/app/page.tsx
@@ -7,10 +7,57 @@ import {
   MovieHeader,
   MoviesContainer,
 } from "@/components/myComponents";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 import Link from "next/link";
 
-export default function Home() {
+type MovieType = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  vote_average: number;
+};
+
+type movieResponseType = {
+  page: number;
+  totalPages: number;
+  results: MovieType[];
+};
+
+export default async function Home() {
+  const getUpcomingMovies = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.TMDB_ACCESS_KEY}`,
+        },
+      }
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const upcomingMovies: movieResponseType = await getUpcomingMovies();
+
+  console.log(upcomingMovies);
+
   const movieArray = [
     {
       movieName: "Dear Santa",
